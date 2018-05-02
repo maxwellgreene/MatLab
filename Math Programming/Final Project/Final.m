ffa991d12004ebc16aa4dceb2523e4d03cb33675
maxwellgreene <32754741+maxwellgreene@users.noreply.github.com>
2018-05-01 23:58:35 -0400

    Moved out of folder

--- Math Programming/Final Project/Final.m
+++ Math Programming/Final Project/Final.m
@@ -2,7 +2,7 @@
 %Maxwell Greene
 %% VARIABLE DEFINITION
 
-global sData; global gData; global speedData;
+global sData gData speedData;
 sData = csvread('sData.csv');
 gData = csvread('gData.csv');
 speedData = csvread('speedData.csv');
@@ -14,27 +14,28 @@ plot(sData(:,1),sData(:,2),'.r');
 plot(gData(:,1),gData(:,2),'ob');
 plot(speedData(:,1),speedData(:,2),'*g');
 
-%% GET USED INPUT FOR DEGREE AND GUESSES
+%% GET USER INPUT FOR DEGREE AND GUESSES
 
 evalNum = input('Evaluate error at which exponent of x?  ');
-% A = zeros(1,evalNum);
-% 
-% alphabet = 'abcdefghijklmnopqrstuvwxyz';
-%     for i = 1:evalNum+1
-%         A(i) = input(['Guess for coefficient of x^',num2str(i-1),', ',alphabet(evalNum+2-i),':  ']);
-%     end
-% 
-% [error,value] = ExpEval(gData(:,1),gData(:,2),A);
-% disp(['Error for guess of x^',num2str(evalNum),': ',num2str(error)]);
-% 
-% figure('Name',['Plot of evaluation of guesses with degree ',num2str(evalNum)]);
-% plot(gData(:,1),gData(:,2),'ob'); hold on;
-% plot(gData(:,1),value);
+A = zeros(1,evalNum);
 
+guessbool = input('Would you like to make a guess? ');
+if guessbool == true
+    
+    for i = 1:evalNum+1
+        A(i) = input(['Guess for coefficient of x^',num2str(i-1),':  ']);
+    end
+    
+    [error,value] = ExpEval(gData(:,1),gData(:,2),A);
+    disp(['Error for guess of x^',num2str(evalNum),': ',num2str(error)]);
+
+    figure('Name',['Plot of evaluation of guesses with degree ',num2str(evalNum)]);
+    plot(gData(:,1),gData(:,2),'ob'); hold on;
+    plot(gData(:,1),value);
+end
 %% CALCULATE OPTIMIZED VALUES
 
-x0 = Inf(1,evalNum+1);
-x0 = 
+x0 = zeros(1,evalNum+1);
 optInputs = fminsearch(@errorFunc,x0);
 [optError,optValue] = ExpEval(gData(:,1),gData(:,2),optInputs);
 disp(['Error for optimization of x^',num2str(evalNum),': ',num2str(optError)]);
@@ -42,12 +43,9 @@ disp(['Error for optimization of x^',num2str(evalNum),': ',num2str(optError)]);
 %Display optimum values
 disp('Your optimum values were:');
 for i=1:evalNum+1
-   disp(['A(',num2str(i),') = ',num2str(optValue(i))]); 
+   disp(['A(',num2str(i),') = ',num2str(optValue(i))]);
 end
 
 figure('Name',['Plot of evaluation of optimization with degree ',num2str(evalNum)]);
 plot(gData(:,1),gData(:,2),'ob'); hold on;
 plot(gData(:,1),optValue);
-
-
-
--- Math Programming/Final Project/errorFunc.m
+++ Math Programming/Final Project/errorFunc.m
@@ -1,16 +1,15 @@
 function error = errorFunc(guess)
-    gData = csvread('gData.csv');
+    global gData;
     data = gData(:,2);
     x = gData(:,1);
     [dataSize,~] = size(data);
-    [~,aSize] = size(guess);
+    [~,guessSize] = size(guess);
     value = zeros(size(x));
     
     for j = 1:dataSize
-        for i = 1:aSize
+        for i = 1:guessSize
             value(j) = value(j) + guess(i).*(x(j).^(i-1));
         end
     end
     error = norm(value-data)^2;
-
 end
